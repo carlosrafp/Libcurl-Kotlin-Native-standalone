bf10f89fb5f2f8c2e37c8e3f9295236ea4e6d0e5
carlosrafp <72582471+carlosrafp@users.noreply.github.com>
2020-11-26 19:16:14 -0300

    Update OutputStream.kt

--- src/nativeMain/kotlin/io/streams/OutputStream.kt
+++ src/nativeMain/kotlin/io/streams/OutputStream.kt
@@ -18,35 +18,26 @@ open class OutputStream {
         this.append = append
         if (append) file = File(path).apply { validate }
         else file = File(path)
-        //if (append) this.fd = fopen(file.absolutePath, "ab")
         if (append && file.exists) this.fd = fopen(file.absolutePath, "r+b")
-        //else if (file.exists) this.fd = fopen(file.absolutePath, "r+b")
         else this.fd = fopen(file.absolutePath, "w+b")
-        //this.fd = open(file.absolutePath, O_RDWR or O_CREAT)
         if (this.fd == null)
             throw FileNotFoundException(this.file.absolutePath, "Cannot acess file")
-        //this.fd = open(file.absolutePath, O_CREAT)
         if (append) {
             this.contentSize = (nativeHeap.alloc<stat>().apply { stat(file.absolutePath, this.ptr) }.st_size -1).toLong()
-            seek(contentSize)
+            seek(contentSize) // to simulate fopen "ab" mode, without the restriction to write only on the end of the file stream
         }
         else this.pos = 0L
     }
 
     constructor(file: File, append: Boolean = false): this(file.absolutePath, append)
 
-    //private var fd: Int = open(file.absolutePath, O_RDWR  or O_CREAT)
-    //private var contentSize = nativeHeap.alloc<stat>().apply { stat(file.absolutePath, this.ptr) }.st_size -1
-
     private fun writeBytes(bytes: ByteArray): Long {
         return writeBytes(bytes,bytes.size)
     }
 
     private fun writeBytes(bytes: ByteArray, len: Int): Long {
-        //if (offset>0) skipCurr(offset)
         return bytes.usePinned {
             val numBytes = fwrite(it.addressOf(0), 1, len.convert(),fd).toLong()
-            //println ("pos = $pos, contentsize = $contentSize")
             if (pos == contentSize) contentSize += numBytes
             else if (pos + numBytes > contentSize ) contentSize = pos + numBytes
             pos += numBytes
@@ -60,23 +51,22 @@ open class OutputStream {
     fun writefromOffset(bytes: ByteArray, offset: Long): Long {
         return writefromOffset(bytes,offset,bytes.size)
     }
-    fun writefromOffset(bytes: ByteArray, offset: Long, len: Int): Long {
-        if (append)
-            throw FileNotFoundException(this.file.absolutePath, "Cannot modify file in append mode")
-        seek(offset)
+    fun writefromOffset(bytes: ByteArray, Destoffset: Long, len: Int): Long {
+        // allows writing from DestOffset even in append mode
+        seek(Destoffset)
         writeBytes(bytes,len)
         this.pos = contentSize
         return seek(pos)
     }
 
     fun seek(n: Long):Long {
+        if (n > contentSize) // cannot seek beyond file size
+            throw IndexOutOfBoundsException("Cannot seek beyond file size")
         if (fseek(this.fd, n.convert(), SEEK_SET) !=0 )
             throw FileNotFoundException(this.file.absolutePath, "fseek error")
         this.pos = n
         return n
     }
-
-    //fun seek(n: Long): Long = fseek(this.fd, n.convert(), SEEK_CUR).toLong()
 
     fun close() = fclose(this.fd)
 
