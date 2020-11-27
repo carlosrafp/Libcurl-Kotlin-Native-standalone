72b4da1320763a1fc5e71ab48a53f3b37dbe87f5
carlosrafp <72582471+carlosrafp@users.noreply.github.com>
2020-11-27 09:59:24 -0300

    Update main.kt
    Using AdaptiveBuffer

--- src/nativeMain/kotlin/main.kt
+++ src/nativeMain/kotlin/main.kt
@@ -11,22 +11,21 @@ import zlib.gzopen
 import zlib.gzprintf
 import libcrypto.*
 import platform.posix.free
-import kotlin.native.identityHashCode
 import kotlin.random.Random
 import kotlin.random.nextUBytes
 
 fun main() {
 
 
-/// AdaptiveBuffer and ObjectOutputStream test
+    /// AdaptiveBuffer and ObjectOutputStream test
     val buf = AdaptiveBuffer()
     val buf2 = ByteArray(4)
     buf2.setIntAt(0,1634951491)
     val a = ObjectOutputStream(buf)
-    a.writeInt(1634951491)   // equivalente a "casa"
-    //a.putShort(24899)    // equivalente a "ca"...
-    //a.putShort(24947)    // equivalente a ..."sa"
-    //a.putFloat(2.8059795E20F)  // equivalente a "casa"
+    a.writeInt(1634951491)   // same as "casa"
+    //a.putShort(24899)    // same as "ca"...
+    //a.putShort(24947)    // same as ..."sa"
+    //a.putFloat(2.8059795E20F)  // same as "casa"
     println("float = ${a.toByteArray().getFloatAt(0)}")
     println("int = ${a.toByteArray().getIntAt(0)}")
     val b = ObjectInputStream(buf)
@@ -56,8 +55,6 @@ fun main() {
     var site: String
     try{
         val arq = InputStream("link.txt")
-        //val link = ByteArray(arq.available().toInt())
-        //arq.read(link)
         val link = arq.readAll()
         site = link.decodeToString()
         println("link = $link")
@@ -87,34 +84,15 @@ fun main() {
     arq.write(msg_in)
     arq.write("\r\n".encodeToByteArray())
 
-
-    // encriptografa independente no numero de bytes para o mesmo numero de bytes finais, logo "resto" nao eh necessario
-    //val len = msg_in.size
-    //val resto = if (len % 16 > 0) 16 - (len % 16) else 16
-    //val resto = 0
-    //val msg_in = UByteArray(len + resto)
-    //val msg_in = UByteArray(len)
-    //for (i in 0 until len)
-    //    msg_in[i] = msg.encodeToByteArray()[i].toUByte()
-    //for (i in len until len + resto)
-    //    msg_in[i] = resto.toUByte()
-    //println("resto deve ser = ${msg_in[len+resto-1].toInt()}")
     val msg_out = UByteArray(msg_in.size)
     AES_cbc_encrypt(msg_in.asUByteArray().refTo(0),msg_out.refTo(0),msg_in.size.convert(),chave.ptr,iv.refTo(0), AES_ENCRYPT)
 
     val msg_out2 = UByteArray(msg_in.size)
-    //println("resultado apos encritografar = ${msg_out.toByteArray().decodeToString()}")
     arq.write(msg_out.asByteArray())
     arq.write("\r\n".encodeToByteArray())
 
     AES_set_decrypt_key(key.refTo(0), 128, chave.ptr); // a chave de desencriptografia nao serve para encriptografia e o iv deve ser novo
     AES_cbc_encrypt(msg_out.refTo(0),msg_out2.refTo(0),msg_out.size.convert(),chave.ptr,iv2.refTo(0), AES_DECRYPT)
-    //val len2 = msg_out2.size
-    //println("chegou ate aqui,len2 = ${msg_out.size}")
-    //val resto2 = msg_out2[len2-1].toInt()
-    //for (i in (len2-resto2) until len2)
-    //    msg_out2[i] = 0.toUByte()
-    //println("resultado depois = ${msg_out2.toByteArray().toKString()}")
 
     arq.write(msg_out2.asByteArray())
     arq.write("\r\n".encodeToByteArray())
@@ -126,20 +104,28 @@ fun main() {
     arq.close()
     nativeHeap.free(chave)
 
-    //val teste = OPENSSL_hexchar2i,nt('c'.toByte().toUByte())
-    //println(teste)
 
     //val site = "https://jonnyzzz.com/blog/2018/10/29/kn-libcurl-windows/"
     println("trying $site")
 
-    ///////////////libcurl test
+    ///////////////libcurl test, using AdaptiveBuffer
+    
+    val cUrlBuf = AdaptiveBuffer()  // buffer to receive cUrl data
+    val bufStableRef = StableRef.create(cUrlBuf)  // stableRef to pass CPointer to cUrl
+    val callback = staticCFunction {  // callback function to retrieve data
+        contents: CPointer<ByteVar>?,
+        size: platform.posix.size_t,
+        nmemb: platform.posix.size_t,
+        userp: COpaquePointer? ->
 
-    //val chunk = retornaStruct() // struct que recebera os dados de CURL
-    val chunk = nativeHeap.alloc<MemoryStruct>() // tres linhas substituem a anterior
-    chunk.size = 0.convert()
-    //chunk.memory = alocarString(1)
-    chunk.memory = nativeHeap.allocArray<ByteVar>(1)
-    //val callback = supply_fun()
+        val numBytes = (nmemb * size) //num of byte to read
+        if (userp != null && contents != null){
+            val bufCurl = userp.asStableRef<AdaptiveBuffer>().get()  //convert void* to AdaptiveBuffer
+            bufCurl.write(contents.readBytes(numBytes.toInt())) // readBytes from Cpointer<ByteVar>
+            numBytes // num bytes written
+        }
+        else 0.convert()  //nothing was read
+    }
 
     val curl = curl_easy_init()
     if (curl != null) {
@@ -149,9 +135,8 @@ fun main() {
         //curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, false)
         curl_easy_setopt(curl, CURLOPT_FRESH_CONNECT, 1L)
 
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, supply_fun());
-        //curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, chunk.ptr); // nativeHeap.alloc permite o ptr
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback); // callback function to write received data
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, bufStableRef.asCPointer()); // passing AdaptiveBuffer to Cfuntion
         curl_easy_setopt(curl, CURLOPT_USERAGENT, "mozilla/5.0");
 
         curl_easy_setopt(curl, CURLOPT_CAINFO, "cacert.pem");
@@ -160,17 +145,57 @@ fun main() {
         if (res != CURLE_OK) {
             println("curl_easy_perform() failed ${curl_easy_strerror(res)?.toKString()}")
         }
-        //val tam = bufsize(chunk).toInt()
-        val tam = chunk.size.toInt()
-        val resposta = ByteArray(tam)
-        copiarBuf(chunk.ptr,resposta.refTo(0))
-        println("lidos ${tam.toInt()} bytes")
+        println("${cUrlBuf.size()} bytes read")
+        val response = cUrlBuf.toByteArray()
         arq = OutputStream("resp.txt")
-        arq.write(resposta)
+        arq.write(response)
         arq.close()
-        //liberarStruct(chunk)
-        nativeHeap.free(chunk)
-
         curl_easy_cleanup(curl)
     }
+
+     
+
+   /*  // same using c function calls/interop (see libcurl.def)
+
+    //val chunk = returnStruct() // struct to receive data, replace the next three lines
+    val chunk = nativeHeap.alloc<MemoryStruct>() // tres linhas substituem a anterior
+    chunk.size = 0.convert()
+    //chunk.memory = allocateString(1) // same as nextline
+    chunk.memory = nativeHeap.allocArray<ByteVar>(1)
+    
+    val callback = supply_fun()  // using C callback function
+
+    val curl = curl_easy_init()
+    if (curl != null) {
+        curl_easy_setopt(curl, CURLOPT_URL, site)
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L)
+        //curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
+        //curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, false)
+        curl_easy_setopt(curl, CURLOPT_FRESH_CONNECT, 1L)
+
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, chunk.ptr); // nativeHeap.alloc allows  o ptr
+        //curl_easy_setopt(curl, CURLOPT_WRITEDATA, chunk); // if using returnStruct()
+        curl_easy_setopt(curl, CURLOPT_USERAGENT, "mozilla/5.0");
+
+        curl_easy_setopt(curl, CURLOPT_CAINFO, "cacert.pem");
+        //curl_easy_setopt(curl, CURLOPT_CAPATH, "\\.");
+        val res = curl_easy_perform(curl)
+        if (res != CURLE_OK) {
+            println("curl_easy_perform() failed ${curl_easy_strerror(res)?.toKString()}")
+        }
+        //val tam = bufsize(chunk).toInt()  // if using returnStruct()
+        val tam = chunk.size.toInt()
+        val response = ByteArray(tam)
+        copyBuf(chunk.ptr,response.refTo(0))
+        println("${tam.toInt()} bytes read")
+        arq = OutputStream("resp.txt")
+        arq.write(response)
+        arq.close()
+        //freeStruct(chunk)   // if using returnStruct()
+        nativeHeap.free(chunk)
+        curl_easy_cleanup(curl)
+    }
+   */
+
 }
